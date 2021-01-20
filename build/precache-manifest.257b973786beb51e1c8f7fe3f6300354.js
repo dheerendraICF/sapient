46f46f91fe3460ffc42c30c6b4a7c324b732d756
dheerendra <Dheerendra.Singh@icfnext.com>
2021-01-20 22:12:42 +0530

    deploy setup for Github Pages

---
+++ build/precache-manifest.257b973786beb51e1c8f7fe3f6300354.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499a0f1ca862f0b50f3cf3742dcda238",
+    "url": "/sapient/index.html"
+  },
+  {
+    "revision": "1521289e92b7c4a5dbc5",
+    "url": "/sapient/static/css/main.09fd1355.chunk.css"
+  },
+  {
+    "revision": "96a3d652984e622434c9",
+    "url": "/sapient/static/js/2.aade3edc.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/sapient/static/js/2.aade3edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1521289e92b7c4a5dbc5",
+    "url": "/sapient/static/js/main.8463d365.chunk.js"
+  },
+  {
+    "revision": "e24cb85c368482d04c8e",
+    "url": "/sapient/static/js/runtime-main.34e1fa14.js"
+  }
+]);
