92c6e5f1f6b5087a90337591f9f8956002f79e8b
dheerendra <Dheerendra.Singh@icfnext.com>
2021-01-20 22:04:56 +0530

    Updates

---
+++ precache-manifest.ed6a1be5e30d1470fd07abc1f11e03d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1b750a6f3009a2889c24e3ac4e6b0b",
+    "url": "/sapient/index.html"
+  },
+  {
+    "revision": "98a30191430baff23904",
+    "url": "/sapient/static/css/main.0dc92a18.chunk.css"
+  },
+  {
+    "revision": "96a3d652984e622434c9",
+    "url": "/sapient/static/js/2.aade3edc.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/sapient/static/js/2.aade3edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a30191430baff23904",
+    "url": "/sapient/static/js/main.8463d365.chunk.js"
+  },
+  {
+    "revision": "e24cb85c368482d04c8e",
+    "url": "/sapient/static/js/runtime-main.34e1fa14.js"
+  }
+]);
